0d464ed876ccef18dd81aaea4aa67705d68126b6
petercolella <peter.joseph.colella@gmail.com>
2021-02-17 23:09:32 -0500

    employee directory

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Wikipedia API
-
 export default {
-  searchTerms: function(query) {
-    return axios.get(
-      "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
-        query +
-        "&limit=1&format=json&origin=*"
-    );
+  // Gets all users
+  getUsers: function() {
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
